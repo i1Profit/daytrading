7e0bca772d22a008f81db50f7bf0d89a53b0baa4
Michael Stoltz <rushedpro@gmail.com>
2020-05-22 20:21:24 -0400

    Update EMA_Crossover_STUDY.ts

--- EMA_Crossover_STUDY.ts
+++ EMA_Crossover_STUDY.ts
@@ -1,22 +1,20 @@
-
 declare lower;
 
 input emaLength = 21;
 
-# define the exponential averge of the close
-def emaValue = ExpAverage(close, emaLength); 
+# EMA of close
+def emaValue = ExpAverage(close, emaLength);
 
-# define a signal that equals one if the previous bar close is below the
-# emaValue while the current bar close is above the emaValue
+# EMAValue while current close is above
 def priceCrossAboveEMA = close[1] < emaValue[1] and close > emaValue;
 
-# plot a signal line that will equal a value of 1 when the 
-# condition is found, zero if not found
+# plot line equal to 1 when condition true, else zero
 plot Scan = priceCrossAboveEMA;
 
-assignBackgroundColor(if Scan >= 1 then color.WHITE else color.BLACK);
+assignBackgroundColor(
+    if Scan >= 1 then color.WHITE
+    else color.BLACK);
 
-scan.assignvaluecolor(if priceCrossAboveEMA >= 1 then color.BLACK else color.WHITE);
-
-
- 
+scan.assignvaluecolor(
+    if priceCrossAboveEMA >= 1 then color.BLACK
+    else color.WHITE);
